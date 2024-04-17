c7193dc4abf3b094ad9685e1a89f8d0e4545c154
Samuel <samuelketema01@gmail.com>
2024-04-17 13:43:42 +0300

    new itemmodel class and associated properties with item_model

---
+++ lib/src/models/item_model.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+
+class ItemModel{
+late final int id;
+late final bool deleted;
+late final String type;
+late final String by;
+late final int time	;
+late final String text	;
+late final bool dead	;
+late final int parent	;
+late final List<dynamic> kids	;
+late final String url;
+late final int score 	;
+late final String title;
+late final int descendants ;
+
+
+}
