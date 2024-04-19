35369d1bab1b3ee645bca1a931b1c99aa830f208
Samuel <samuelketema01@gmail.com>
2024-04-19 21:16:05 +0300

    Testing is implemetned for the fetchTopIds function.

--- test/lib/src/models/resources/api_provider_test.dart
+++ test/lib/src/models/resources/api_provider_test.dart
@@ -1,5 +1,19 @@
 import 'dart:convert';
+import 'package:flutter_test/flutter_test.dart';
 import 'package:http/http.dart';
 import 'package:news/src/models/resources/api_provider.dart';
 import 'package:http/testing.dart';
 
+void main(){
+  test('Testing the fetchTopIds function of NewApiProvider class' , () async {
+  final newsApi = NewApiProvider();
+
+    newsApi.client = MockClient(  (request) async{
+      return Response(json.encode([1,2,3,4,5]), 10);
+    });
+
+    var ids =  await newsApi.fetchItem();
+
+    expect(ids, [1,2,3,4,5]);
+  } );
+}
