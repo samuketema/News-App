83cf64a030fa464403ac63a689d97adba6d7323c
Samuel <samuelketema01@gmail.com>
2024-04-21 01:00:05 +0300

    passed a sample argument for the fetchItem function

--- lib/src/models/item_model.dart
+++ lib/src/models/item_model.dart
@@ -2,19 +2,19 @@ import 'package:flutter/material.dart';
 
 
 class ItemModel{
-late final int id;
-late final bool deleted;
-late final String type;
-late final String by;
-late final int time	;
-late final String text	;
-late final bool dead	;
-late final int parent	;
-late final List<dynamic> kids	;
-late final String url;
-late final int score 	;
-late final String title;
-late final int descendants ;
+late final int? id;
+late final bool? deleted;
+late final String? type;
+late final String? by;
+late final int? time	;
+late final String? text	;
+late final bool? dead	;
+late final int? parent	;
+late final List<dynamic>? kids	;
+late final String? url;
+late final int? score 	;
+late final String? title;
+late final int? descendants ;
 
 ItemModel.fromJson(Map<String,dynamic> parsedjson)
 : id = parsedjson['id'],
