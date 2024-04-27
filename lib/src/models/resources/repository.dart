a5bc4cdfd0416019698d0980926c5f8a697f2a8e
Samuel <samuelketema01@gmail.com>
2024-04-27 13:41:48 +0300

    Implementing the repository for testing NewsDbProvider class

---
+++ lib/src/models/resources/repository.dart
@@ -0,0 +1,18 @@
+
+import 'api_provider.dart';
+import 'news_db_provider.dart';
+import '../item_model.dart';
+import 'dart:async';
+
+class Repository{
+  NewsDbProvier dbProvier = NewsDbProvier();
+  NewApiProvider apiProvider = NewApiProvider();
+
+  fetchTopIds(){
+
+  }
+  
+  fetchItem(){
+    
+  }
+}
