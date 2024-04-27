a1b584c3ed0a733ddcd1346f8ef62a8ba99037c7
Samuel <samuelketema01@gmail.com>
2024-04-27 11:01:09 +0300

    Database provider class that provide link to sqlDatabase that we gonna to store in our device.

---
+++ lib/src/models/resources/news_db_provider.dart
@@ -0,0 +1,11 @@
+import 'package:sqflite/sqflite.dart';
+import 'package:path_provider/path_provider.dart';
+import 'dart:io';
+import 'package:path/path.dart';
+import 'dart:async';
+import '../item_model.dart';
+
+class NewsDbProvier {
+  Database db;
+  
+}
