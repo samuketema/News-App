bc6a1d51ea775d632d1a2c61217373f321e6af7a
Samuel <samuelketema01@gmail.com>
2024-05-01 00:00:37 +0300

    Type annotation

--- lib/src/models/resources/news_db_provider.dart
+++ lib/src/models/resources/news_db_provider.dart
@@ -8,7 +8,7 @@ import '../item_model.dart';
 class NewsDbProvier {
   late Database db;
 
-  init() async {
+  void init() async {
     Directory documentDirectory = await getApplicationDocumentsDirectory();
     final path = join(documentDirectory.path, "items.db");
     db = await openDatabase(
@@ -37,7 +37,7 @@ class NewsDbProvier {
       },
       );
   }
-  fetchItem(int id) async{
+  Future<ItemModel?> fetchItem(int id) async{
     final maps = await db.query(
       "Items",
       columns: null,
@@ -52,7 +52,7 @@ class NewsDbProvier {
       return null;
     }
   }
-  addItem(ItemModel item){
+   addItem(ItemModel item){
     return db.insert("items", item.toMap());
   }
 }
