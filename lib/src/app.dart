6cd3ee2b8a46bb55e2256e78d35292d626e07dd5
Samuel <samuelketema01@gmail.com>
2024-05-04 21:38:39 +0300

    Wiring up the stories provider

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
 import 'screens/newslist.dart';
+import 'blocs/stories-provider.dart';
 
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return StoryProvider(
+      child:MaterialApp(
       title: "NewStory App",
       home: NewsList(),
+    )
     );
+    
   }
 }
