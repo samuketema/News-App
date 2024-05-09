0a97108af0482651af56817489a6ade57d2c08d2
Samuel <samuelketema01@gmail.com>
2024-05-09 13:46:10 +0300

    implementing the stream transformer

--- lib/src/screens/newslist.dart
+++ lib/src/screens/newslist.dart
@@ -1,10 +1,33 @@
 import 'package:flutter/material.dart';
+import '../blocs/stories-provider.dart';
 class NewsList extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
+    final bloc = StoryProvider.of(context);
+
+    bloc.fetchTopIds();
     return Scaffold(
       appBar: AppBar(title: Text('Top News')),
-      body: Text('show some news') 
+      body: buildList(bloc)
 );
   }
+Widget buildList(StoriesBloc bloc){
+  return StreamBuilder(
+    stream: bloc.topIds,
+    builder: (context, AsyncSnapshot<List<int>?> snapshot){
+      if (!snapshot.hasData){
+      return Text('still waiting on Ids');
+
+      }
+    
+    
+    return ListView.builder(
+      itemCount:snapshot.data?.length ?? 0,
+      itemBuilder:(context,int index){
+        return Text(snapshot.data?[index].toString() ?? " ");
+      },
+    );
+}
+    );
+}
 }
