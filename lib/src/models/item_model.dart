dfd409365e7f42c97f4bd9d5a242c3020bbc1611
Samuel <samuelketema01@gmail.com>
2024-05-15 15:50:19 +0300

    Applying null check to the descendants

--- lib/src/models/item_model.dart
+++ lib/src/models/item_model.dart
@@ -32,7 +32,7 @@ class ItemModel {
         url = parsedJson['url'],
         score = parsedJson['score'],
         title = parsedJson['title'],
-        descendants = parsedJson['descendants'];
+        descendants = parsedJson['descendants'] ?? 0;
 
   ItemModel.fromDb(Map<String, dynamic> parsedJson)
       : id = parsedJson['id'],
