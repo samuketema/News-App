c65e9bace9373ced7fc0e3836e3d33482ef2c739
Samuel <samuelketema01@gmail.com>
2024-05-16 16:17:51 +0300

    New loding continer.

--- lib/src/widgets/loading_container.dart
+++ lib/src/widgets/loading_container.dart
@@ -2,6 +2,23 @@ import 'package:flutter/material.dart';
 
 class LoadingContainer extends StatelessWidget{
 Widget  build(BuildContext context){
-    return  ;
+    return Column(
+      children: [
+        ListTile(
+          title: buildContainer() ,
+          subtitle:buildContainer() ,
+        ),
+        Divider(height:8.0)
+    ],) ;
   }
+
+  Widget buildContainer(){
+    return Container(
+      color: Colors.grey[200],
+      height:24 ,
+      width: 124,
+      margin: EdgeInsets.only(top: 5,bottom: 5),
+    );
+  }
+  
 }
